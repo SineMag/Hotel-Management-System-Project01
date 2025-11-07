4aa4dbba9b54b66637debd12ae049e2058427868
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-07 16:49:53 +0200

    doc: created the server-related documents

---
+++ server/index.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hotel Management System API is running...');
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Connection error', err);
+  });
